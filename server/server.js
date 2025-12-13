bd60586c7f3193eeb034fed6ce928c8d29d3697b
niranjanms0005-cloud <niranjanms05@gmail.com>
2025-12-13 13:03:22 +0530

    first commit

---
+++ server/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Connect to database
+connectDB();
+
+// Routes
+app.get('/', (req, res) => {
+  res.json({ message: 'API is running' });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
